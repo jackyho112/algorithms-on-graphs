6b4e1fb50fc603c076b7ce345fb6b6c23b99315b
jackyho112 <jackyho112dev@gmail.com>
2017-10-21 15:24:50 -0700

    Achieve correctness but not efficiency

--- bfs.py
+++ bfs.py
@@ -3,9 +3,51 @@
 import sys
 import queue
 
+def bfs(adj, s):
+    dist = {}
+    prev = {}
+    localQueue = queue.Queue()
+
+    for i in range(len(adj)):
+        dist[i] = False
+        prev[i] = []
+
+    dist[s] = 0
+    localQueue.put(s)
+
+    while not localQueue.empty():
+        current = localQueue.get()
+        for nextNode in adj[current]:
+            if dist[nextNode] == False:
+                localQueue.put(nextNode)
+                dist[nextNode] = dist[current] + 1
+                prev[nextNode].append(current)
+
+    return prev
+
+def find_distance(prev, s, t):
+    if not prev[t]:
+        return -1
+
+    distance = 0
+    result = []
+    current = [t]
+    while s not in result:
+        local_current = []
+        distance += 1
+
+        for last in current:
+            result.append(last)
+            local_current += prev[last]
+
+        current = local_current
+
+    return distance - 1
+
 def distance(adj, s, t):
-    #write your code here
-    return -1
+    prev = bfs(adj, s)
+
+    return find_distance(prev, s, t)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
