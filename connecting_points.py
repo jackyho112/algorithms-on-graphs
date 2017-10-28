587cef42cdb35f5f85b5961fd531a6d15ef98d4d
jackyho112 <jackyho112dev@gmail.com>
2017-10-28 15:45:26 -0700

    Finish connecting points with minimum distance

--- connecting_points.py
+++ connecting_points.py
@@ -2,10 +2,37 @@
 import sys
 import math
 
+def get_length_between(x, y):
+    return ((x[0] - x[1])**2 + (y[0] - y[1])**2)**.5
+
+def extract_min(queue, cost):
+    node = min(queue, key=lambda x: cost[x])
+
+    queue.remove(node)
+
+    return node
+
 def minimum_distance(x, y):
-    result = 0.
-    #write your code here
-    return result
+    cost = {}
+    queue = []
+
+    for i in range(len(x)):
+        cost[i] = float("inf")
+        queue.append(i)
+
+    cost[0] = 0
+
+    while queue:
+        current_node = extract_min(queue, cost)
+        for node in queue:
+            new_cost = get_length_between(
+                [x[current_node], x[node]], [y[current_node], y[node]]
+            )
+
+            if new_cost < cost[node]:
+                cost[node] = new_cost
+
+    return sum(cost.values())
 
 
 if __name__ == '__main__':
